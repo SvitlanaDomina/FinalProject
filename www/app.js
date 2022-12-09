3f987332b385d15e0325d8647d3d6bea96ad4ae3
Svitlana Domina <sfill444@gmail.com>
2022-12-09 17:27:42 +0100

    MyFinalProject

---
+++ www/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Change BG";
+    } else {
+        this.textContent = "Back BG"
+    }
+    console.log('current class name: ' + className);
+});
